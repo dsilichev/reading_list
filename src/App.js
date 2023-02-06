907ac5f3f7c6633f5bb8b8423f517a45457d5137
Dmitry <dsilichev@gmail.com>
2023-02-06 17:33:48 +0300

    create_book

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Need to add book with title", title);
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.floor(Math.random() * 9999),
+        title}
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
